a4236d0ff740e1bb01cc3cce1692568386020c58
denn02-git <90389000+denn02-git@users.noreply.github.com>
2022-08-21 22:09:33 +0900

    改行の挙動をテスト 　正解版
    セットで使うんやな・・・

--- Hell World.cpp
+++ Hell World.cpp
@@ -3,9 +3,12 @@
 int main()   // ()を忘れて1敗
 {
     std::cout<< "Hell Wolrd " << std::endl;  // "o" をしっかり忘れて無敗　";"を忘れず無敗. end"l" の"l"を忘れて1敗
-	for (i = 0; i <= 4 ;i++)
+	
+	int i = 0;  //宣言忘れで一敗
+    
+	for (i = 0; i <= 4 ;i++)//forの構文忘れて1敗
 	{
-		std::endl;
+		std::cout << std::endl; // 各要素を単体で使おうとして2敗
 	}
 	
 	std::cout<<"(ヽ´ω｀)改行いっぱい"<<std::endl;
